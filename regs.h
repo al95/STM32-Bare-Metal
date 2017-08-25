36d92589ac0e0ab1ed0d3cfe2d2973a4593928f5
al95 <31206029+al95@users.noreply.github.com>
2017-08-25 12:32:39 +0200

    Add bit-banding support

--- regs.h
+++ regs.h
@@ -39,7 +39,6 @@
 
 
 
-
 // *****************************************************************************
 // *                          < Register structures >                          *
 // *****************************************************************************
@@ -599,6 +598,479 @@ typedef struct {
 #define STK           ( (STK_Type*)       STK_BASE    )
 #define SCB           ( (SCB_Type*)       SCB_BASE    )
 #define DBG           ( (DBG_Type*)       DBG_BASE    )
+
+
+
+// *****************************************************************************
+// *                    < Bit-banding register structures >                    *
+// *****************************************************************************
+
+typedef struct {
+  volatile uint32_t CR1[32];
+  volatile uint32_t CR2[32];
+  volatile uint32_t SMCR[32];
+  volatile uint32_t DIER[32];
+  volatile uint32_t SR[32];
+  volatile uint32_t EGR[32];
+  volatile uint32_t CCMR1[32];
+  volatile uint32_t CCMR2[32];
+  volatile uint32_t CCER[32];
+  volatile uint32_t CNT[32];
+  volatile uint32_t PSC[32];
+  volatile uint32_t ARR[32];
+  volatile uint32_t RESERVED_1[32];
+  volatile uint32_t CCR1[32];
+  volatile uint32_t CCR2[32];
+  volatile uint32_t CCR3[32];
+  volatile uint32_t CCR4[32];
+  volatile uint32_t RESERVED_2[32];
+  volatile uint32_t DCR[32];
+  volatile uint32_t DMAR[32];
+} TIM2_BB_Type;
+
+typedef struct {
+  volatile uint32_t CRH[32];
+  volatile uint32_t CRL[32];
+  volatile uint32_t PRLH[32];
+  volatile uint32_t PRLL[32];
+  volatile uint32_t DIVH[32];
+  volatile uint32_t DIVL[32];
+  volatile uint32_t CNTH[32];
+  volatile uint32_t CNTL[32];
+  volatile uint32_t ALRH[32];
+  volatile uint32_t ALRL[32];
+} RTC_BB_Type;
+
+typedef struct {
+  volatile uint32_t CR[32];
+  volatile uint32_t CFR[32];
+  volatile uint32_t SR[32];
+} WWDG_BB_Type;
+
+typedef struct {
+  volatile uint32_t KR[32];
+  volatile uint32_t PR[32];
+  volatile uint32_t RLR[32];
+  volatile uint32_t SR[32];
+} IWDG_BB_Type;
+
+typedef struct {
+  volatile uint32_t CR1[32];
+  volatile uint32_t CR2[32];
+  volatile uint32_t SR[32];
+  volatile uint32_t DR[32];
+  volatile uint32_t CRCPR[32];
+  volatile uint32_t RXCRCR[32];
+  volatile uint32_t TXCRCR[32];
+  volatile uint32_t I2SCFGR[32];
+  volatile uint32_t I2SPR[32];
+} SPI1_BB_Type;
+
+typedef struct {
+  volatile uint32_t SR[32];
+  volatile uint32_t DR[32];
+  volatile uint32_t BRR[32];
+  volatile uint32_t CR1[32];
+  volatile uint32_t CR2[32];
+  volatile uint32_t CR3[32];
+  volatile uint32_t GTPR[32];
+} USART1_BB_Type;
+
+typedef struct {
+  volatile uint32_t CR1[32];
+  volatile uint32_t CR2[32];
+  volatile uint32_t OAR1[32];
+  volatile uint32_t OAR2[32];
+  volatile uint32_t DR[32];
+  volatile uint32_t SR1[32];
+  volatile uint32_t SR2[32];
+  volatile uint32_t CCR[32];
+  volatile uint32_t TRISE[32];
+} I2C1_BB_Type;
+
+typedef struct {
+  volatile uint32_t EP0R[32];
+  volatile uint32_t EP1R[32];
+  volatile uint32_t EP2R[32];
+  volatile uint32_t EP3R[32];
+  volatile uint32_t EP4R[32];
+  volatile uint32_t EP5R[32];
+  volatile uint32_t EP6R[32];
+  volatile uint32_t EP7R[32];
+  volatile uint32_t RESERVED_1[8][32];
+  volatile uint32_t CNTR[32];
+  volatile uint32_t ISTR[32];
+  volatile uint32_t FNR[32];
+  volatile uint32_t DADDR[32];
+  volatile uint32_t BTABLE[32];
+} USB_BB_Type;
+
+typedef struct {
+  volatile uint32_t CAN_MCR[32];
+  volatile uint32_t CAN_MSR[32];
+  volatile uint32_t CAN_TSR[32];
+  volatile uint32_t CAN_RF0R[32];
+  volatile uint32_t CAN_RF1R[32];
+  volatile uint32_t CAN_IER[32];
+  volatile uint32_t CAN_ESR[32];
+  volatile uint32_t CAN_BTR[32];
+  volatile uint32_t RESERVED_1[88][32];
+  volatile uint32_t CAN_TI0R[32];
+  volatile uint32_t CAN_TDT0R[32];
+  volatile uint32_t CAN_TDL0R[32];
+  volatile uint32_t CAN_TDH0R[32];
+  volatile uint32_t CAN_TI1R[32];
+  volatile uint32_t CAN_TDT1R[32];
+  volatile uint32_t CAN_TDL1R[32];
+  volatile uint32_t CAN_TDH1R[32];
+  volatile uint32_t CAN_TI2R[32];
+  volatile uint32_t CAN_TDT2R[32];
+  volatile uint32_t CAN_TDL2R[32];
+  volatile uint32_t CAN_TDH2R[32];
+  volatile uint32_t CAN_RI0R[32];
+  volatile uint32_t CAN_RDT0R[32];
+  volatile uint32_t CAN_RDL0R[32];
+  volatile uint32_t CAN_RDH0R[32];
+  volatile uint32_t CAN_RI1R[32];
+  volatile uint32_t CAN_RDT1R[32];
+  volatile uint32_t CAN_RDL1R[32];
+  volatile uint32_t CAN_RDH1R[32];
+  volatile uint32_t RESERVED_2[12][32];
+  volatile uint32_t CAN_FMR[32];
+  volatile uint32_t CAN_FM1R[32];
+  volatile uint32_t RESERVED_3[32];
+  volatile uint32_t CAN_FS1R[32];
+  volatile uint32_t RESERVED_4[32];
+  volatile uint32_t CAN_FFA1R[32];
+  volatile uint32_t RESERVED_5[32];
+  volatile uint32_t CAN_FA1R[32];
+  volatile uint32_t RESERVED_6[8][32];
+  volatile uint32_t F0R1[32];
+  volatile uint32_t F0R2[32];
+  volatile uint32_t F1R1[32];
+  volatile uint32_t F1R2[32];
+  volatile uint32_t F2R1[32];
+  volatile uint32_t F2R2[32];
+  volatile uint32_t F3R1[32];
+  volatile uint32_t F3R2[32];
+  volatile uint32_t F4R1[32];
+  volatile uint32_t F4R2[32];
+  volatile uint32_t F5R1[32];
+  volatile uint32_t F5R2[32];
+  volatile uint32_t F6R1[32];
+  volatile uint32_t F6R2[32];
+  volatile uint32_t F7R1[32];
+  volatile uint32_t F7R2[32];
+  volatile uint32_t F8R1[32];
+  volatile uint32_t F8R2[32];
+  volatile uint32_t F9R1[32];
+  volatile uint32_t F9R2[32];
+  volatile uint32_t F10R1[32];
+  volatile uint32_t F10R2[32];
+  volatile uint32_t F11R1[32];
+  volatile uint32_t F11R2[32];
+  volatile uint32_t F12R1[32];
+  volatile uint32_t F12R2[32];
+  volatile uint32_t F13R1[32];
+  volatile uint32_t F13R2[32];
+} CAN1_BB_Type;
+
+typedef struct {
+  volatile uint32_t DR1[32];
+  volatile uint32_t DR2[32];
+  volatile uint32_t DR3[32];
+  volatile uint32_t DR4[32];
+  volatile uint32_t DR5[32];
+  volatile uint32_t DR6[32];
+  volatile uint32_t DR7[32];
+  volatile uint32_t DR8[32];
+  volatile uint32_t DR9[32];
+  volatile uint32_t DR10[32];
+  volatile uint32_t RTCCR[32];
+  volatile uint32_t CR[32];
+  volatile uint32_t CSR[32];
+  volatile uint32_t RESERVED_1[2][32];
+  volatile uint32_t DR11[32];
+  volatile uint32_t DR12[32];
+  volatile uint32_t DR13[32];
+  volatile uint32_t DR14[32];
+  volatile uint32_t DR15[32];
+  volatile uint32_t DR16[32];
+  volatile uint32_t DR17[32];
+  volatile uint32_t DR18[32];
+  volatile uint32_t DR19[32];
+  volatile uint32_t DR20[32];
+  volatile uint32_t DR21[32];
+  volatile uint32_t DR22[32];
+  volatile uint32_t DR23[32];
+  volatile uint32_t DR24[32];
+  volatile uint32_t DR25[32];
+  volatile uint32_t DR26[32];
+  volatile uint32_t DR27[32];
+  volatile uint32_t DR28[32];
+  volatile uint32_t DR29[32];
+  volatile uint32_t DR30[32];
+  volatile uint32_t DR31[32];
+  volatile uint32_t DR32[32];
+  volatile uint32_t DR33[32];
+  volatile uint32_t DR34[32];
+  volatile uint32_t DR35[32];
+  volatile uint32_t DR36[32];
+  volatile uint32_t DR37[32];
+  volatile uint32_t DR38[32];
+  volatile uint32_t DR39[32];
+  volatile uint32_t DR40[32];
+  volatile uint32_t DR41[32];
+  volatile uint32_t DR42[32];
+} BKP_BB_Type;
+
+typedef struct {
+  volatile uint32_t CR[32];
+  volatile uint32_t CSR[32];
+} PWR_BB_Type;
+
+typedef struct {
+  volatile uint32_t EVCR[32];
+  volatile uint32_t MAPR[32];
+  volatile uint32_t EXTICR1[32];
+  volatile uint32_t EXTICR2[32];
+  volatile uint32_t EXTICR3[32];
+  volatile uint32_t EXTICR4[32];
+  volatile uint32_t RESERVED_1[32];
+  volatile uint32_t MAPR2[32];
+} AFIO_BB_Type;
+
+typedef struct {
+  volatile uint32_t IMR[32];
+  volatile uint32_t EMR[32];
+  volatile uint32_t RTSR[32];
+  volatile uint32_t FTSR[32];
+  volatile uint32_t SWIER[32];
+  volatile uint32_t PR[32];
+} EXTI_BB_Type;
+
+typedef struct {
+  volatile uint32_t CRL[32];
+  volatile uint32_t CRH[32];
+  volatile uint32_t IDR[32];
+  volatile uint32_t ODR[32];
+  volatile uint32_t BSRR[32];
+  volatile uint32_t BRR[32];
+  volatile uint32_t LCKR[32];
+} GPIOA_BB_Type;
+
+typedef struct {
+  volatile uint32_t SR[32];
+  volatile uint32_t CR1[32];
+  volatile uint32_t CR2[32];
+  volatile uint32_t SMPR1[32];
+  volatile uint32_t SMPR2[32];
+  volatile uint32_t JOFR1[32];
+  volatile uint32_t JOFR2[32];
+  volatile uint32_t JOFR3[32];
+  volatile uint32_t JOFR4[32];
+  volatile uint32_t HTR[32];
+  volatile uint32_t LTR[32];
+  volatile uint32_t SQR1[32];
+  volatile uint32_t SQR2[32];
+  volatile uint32_t SQR3[32];
+  volatile uint32_t JSQR[32];
+  volatile uint32_t JDR1[32];
+  volatile uint32_t JDR2[32];
+  volatile uint32_t JDR3[32];
+  volatile uint32_t JDR4[32];
+  volatile uint32_t DR[32];
+} ADC1_BB_Type;
+
+typedef struct {
+  volatile uint32_t SR[32];
+  volatile uint32_t CR1[32];
+  volatile uint32_t CR2[32];
+  volatile uint32_t SMPR1[32];
+  volatile uint32_t SMPR2[32];
+  volatile uint32_t JOFR1[32];
+  volatile uint32_t JOFR2[32];
+  volatile uint32_t JOFR3[32];
+  volatile uint32_t JOFR4[32];
+  volatile uint32_t HTR[32];
+  volatile uint32_t LTR[32];
+  volatile uint32_t SQR1[32];
+  volatile uint32_t SQR2[32];
+  volatile uint32_t SQR3[32];
+  volatile uint32_t JSQR[32];
+  volatile uint32_t JDR1[32];
+  volatile uint32_t JDR2[32];
+  volatile uint32_t JDR3[32];
+  volatile uint32_t JDR4[32];
+  volatile uint32_t DR[32];
+} ADC2_BB_Type;
+
+typedef struct {
+  volatile uint32_t CR1[32];
+  volatile uint32_t CR2[32];
+  volatile uint32_t SMCR[32];
+  volatile uint32_t DIER[32];
+  volatile uint32_t SR[32];
+  volatile uint32_t EGR[32];
+  volatile uint32_t CCMR1[32];
+  volatile uint32_t CCMR2[32];
+  volatile uint32_t CCER[32];
+  volatile uint32_t CNT[32];
+  volatile uint32_t PSC[32];
+  volatile uint32_t ARR[32];
+  volatile uint32_t RCR[32];
+  volatile uint32_t CCR1[32];
+  volatile uint32_t CCR2[32];
+  volatile uint32_t CCR3[32];
+  volatile uint32_t CCR4[32];
+  volatile uint32_t BDTR[32];
+  volatile uint32_t DCR[32];
+  volatile uint32_t DMAR[32];
+} TIM1_BB_Type;
+
+typedef struct {
+  volatile uint32_t ISR[32];
+  volatile uint32_t IFCR[32];
+  volatile uint32_t CCR1[32];
+  volatile uint32_t CNDTR1[32];
+  volatile uint32_t CPAR1[32];
+  volatile uint32_t CMAR1[32];
+  volatile uint32_t RESERVED_1[32];
+  volatile uint32_t CCR2[32];
+  volatile uint32_t CNDTR2[32];
+  volatile uint32_t CPAR2[32];
+  volatile uint32_t CMAR2[32];
+  volatile uint32_t RESERVED_2[32];
+  volatile uint32_t CCR3[32];
+  volatile uint32_t CNDTR3[32];
+  volatile uint32_t CPAR3[32];
+  volatile uint32_t CMAR3[32];
+  volatile uint32_t RESERVED_3[32];
+  volatile uint32_t CCR4[32];
+  volatile uint32_t CNDTR4[32];
+  volatile uint32_t CPAR4[32];
+  volatile uint32_t CMAR4[32];
+  volatile uint32_t RESERVED_4[32];
+  volatile uint32_t CCR5[32];
+  volatile uint32_t CNDTR5[32];
+  volatile uint32_t CPAR5[32];
+  volatile uint32_t CMAR5[32];
+  volatile uint32_t RESERVED_5[32];
+  volatile uint32_t CCR6[32];
+  volatile uint32_t CNDTR6[32];
+  volatile uint32_t CPAR6[32];
+  volatile uint32_t CMAR6[32];
+  volatile uint32_t RESERVED_6[32];
+  volatile uint32_t CCR7[32];
+  volatile uint32_t CNDTR7[32];
+  volatile uint32_t CPAR7[32];
+  volatile uint32_t CMAR7[32];
+} DMA1_BB_Type;
+
+typedef struct {
+  volatile uint32_t CR[32];
+  volatile uint32_t CFGR[32];
+  volatile uint32_t CIR[32];
+  volatile uint32_t APB2RSTR[32];
+  volatile uint32_t APB1RSTR[32];
+  volatile uint32_t AHBENR[32];
+  volatile uint32_t APB2ENR[32];
+  volatile uint32_t APB1ENR[32];
+  volatile uint32_t BDCR[32];
+  volatile uint32_t CSR[32];
+} RCC_BB_Type;
+
+typedef struct {
+  volatile uint32_t ACR[32];
+  volatile uint32_t KEYR[32];
+  volatile uint32_t OPTKEYR[32];
+  volatile uint32_t SR[32];
+  volatile uint32_t CR[32];
+  volatile uint32_t AR[32];
+  volatile uint32_t RESERVED_1[32];
+  volatile uint32_t OBR[32];
+  volatile uint32_t WRPR[32];
+} FLASH_BB_Type;
+
+typedef struct {
+  volatile uint32_t DR[32];
+  volatile uint32_t IDR[32];
+  volatile uint32_t CR[32];
+} CRC_BB_Type;
+
+
+
+// *****************************************************************************
+// *                      < Bit-banding base addresses >                       *
+// *****************************************************************************
+
+#define TIM2_BB_BASE          ((uint32_t) 0x42000000)
+#define TIM3_BB_BASE          ((uint32_t) 0x42008000)
+#define TIM4_BB_BASE          ((uint32_t) 0x42010000)
+#define RTC_BB_BASE           ((uint32_t) 0x42050000)
+#define WWDG_BB_BASE          ((uint32_t) 0x42058000)
+#define IWDG_BB_BASE          ((uint32_t) 0x42060000)
+#define SPI2_BB_BASE          ((uint32_t) 0x42070000)
+#define USART2_BB_BASE        ((uint32_t) 0x42088000)
+#define USART3_BB_BASE        ((uint32_t) 0x42090000)
+#define I2C1_BB_BASE          ((uint32_t) 0x420A8000)
+#define I2C2_BB_BASE          ((uint32_t) 0x420B0000)
+#define USB_BB_BASE           ((uint32_t) 0x420B8000)
+#define CAN1_BB_BASE          ((uint32_t) 0x420C8000)
+#define BKP_BB_BASE           ((uint32_t) 0x420D8000)
+#define PWR_BB_BASE           ((uint32_t) 0x420E0000)
+#define AFIO_BB_BASE          ((uint32_t) 0x42200000)
+#define EXTI_BB_BASE          ((uint32_t) 0x42208000)
+#define GPIOA_BB_BASE         ((uint32_t) 0x42210000)
+#define GPIOB_BB_BASE         ((uint32_t) 0x42218000)
+#define GPIOC_BB_BASE         ((uint32_t) 0x42220000)
+#define GPIOD_BB_BASE         ((uint32_t) 0x42228000)
+#define ADC1_BB_BASE          ((uint32_t) 0x42248000)
+#define ADC2_BB_BASE          ((uint32_t) 0x42250000)
+#define TIM1_BB_BASE          ((uint32_t) 0x42258000)
+#define SPI1_BB_BASE          ((uint32_t) 0x42260000)
+#define USART1_BB_BASE        ((uint32_t) 0x42270000)
+#define DMA1_BB_BASE          ((uint32_t) 0x42400000)
+#define RCC_BB_BASE           ((uint32_t) 0x42420000)
+#define FLASH_BB_BASE         ((uint32_t) 0x42440000)
+#define CRC_BB_BASE           ((uint32_t) 0x42460000)
+
+
+
+// *****************************************************************************
+// *                   < Bit-banding locations assignment >                    *
+// *****************************************************************************
+
+#define BB_TIM2          ( (TIM2_BB_Type*)      TIM2_BB_BASE   )
+#define BB_TIM3          ( (TIM2_BB_Type*)      TIM3_BB_BASE   )
+#define BB_TIM4          ( (TIM2_BB_Type*)      TIM4_BB_BASE   )
+#define BB_RTC           ( (RTC_BB_Type*)       RTC_BB_BASE    )
+#define BB_WWDG          ( (WWDG_BB_Type*)      WWDG_BB_BASE   )
+#define BB_IWDG          ( (IWDG_BB_Type*)      IWDG_BB_BASE   )
+#define BB_SPI2          ( (SPI1_BB_Type*)      SPI2_BB_BASE   )
+#define BB_USART2        ( (USART1_BB_Type*)    USART2_BB_BASE )
+#define BB_USART3        ( (USART1_BB_Type*)    USART3_BB_BASE )
+#define BB_I2C1          ( (I2C1_BB_Type*)      I2C1_BB_BASE   )
+#define BB_I2C2          ( (I2C1_BB_Type*)      I2C2_BB_BASE   )
+#define BB_USB           ( (USB_BB_Type*)       USB_BB_BASE    )
+#define BB_CAN1          ( (CAN1_BB_Type*)      CAN1_BB_BASE   )
+#define BB_BKP           ( (BKP_BB_Type*)       BKP_BB_BASE    )
+#define BB_PWR           ( (PWR_BB_Type*)       PWR_BB_BASE    )
+#define BB_AFIO          ( (AFIO_BB_Type*)      AFIO_BB_BASE   )
+#define BB_EXTI          ( (EXTI_BB_Type*)      EXTI_BB_BASE   )
+#define BB_GPIOA         ( (GPIOA_BB_Type*)     GPIOA_BB_BASE  )
+#define BB_GPIOB         ( (GPIOA_BB_Type*)     GPIOB_BB_BASE  )
+#define BB_GPIOC         ( (GPIOA_BB_Type*)     GPIOC_BB_BASE  )
+#define BB_GPIOD         ( (GPIOA_BB_Type*)     GPIOD_BB_BASE  )
+#define BB_ADC1          ( (ADC1_BB_Type*)      ADC1_BB_BASE   )
+#define BB_ADC2          ( (ADC2_BB_Type*)      ADC2_BB_BASE   )
+#define BB_TIM1          ( (TIM1_BB_Type*)      TIM1_BB_BASE   )
+#define BB_SPI1          ( (SPI1_BB_Type*)      SPI1_BB_BASE   )
+#define BB_USART1        ( (USART1_BB_Type*)    USART1_BB_BASE )
+#define BB_DMA1          ( (DMA1_BB_Type*)      DMA1_BB_BASE   )
+#define BB_RCC           ( (RCC_BB_Type*)       RCC_BB_BASE    )
+#define BB_FLASH         ( (FLASH_BB_Type*)     FLASH_BB_BASE  )
+#define BB_CRC           ( (CRC_BB_Type*)       CRC_BB_BASE    )
 
 
 
